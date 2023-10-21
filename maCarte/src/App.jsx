bee5f84f5868865699365e237698f1f951d33809
FREDERIC TICON <fred.ticon@gmail.com>
2023-10-21 19:13:49 +0200

    Update App.jsx

--- maCarte/src/App.jsx
+++ maCarte/src/App.jsx
@@ -1,12 +1,22 @@
-
+import React from 'react';
 import './App.css'
+import './style.scss';
 
 function App() {
-  
-
   return (
-   <h1>hhhhhhhhhhhhhhhhhhhh</h1>
-  )
+    <div>
+      <nav>
+        {/* use the class list to remove bullet points and align items side-by-side */}
+        <ul>
+          <li>hhhhhh</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      {/* use the class title to change the title color */}
+      <h1>CSS Modules!</h1>
+      <p>This is an example of CSS Modules!</p>
+    </div>
+  );
 }
-
 export default App
