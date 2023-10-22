cfd3113eaffdf54fa1b4cd44cfe5b3db988db943
FREDERIC TICON <fred.ticon@gmail.com>
2023-10-22 09:07:27 +0200

    Update App.jsx

--- maCarte/src/App.jsx
+++ maCarte/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css'
 import './style.scss';
 
 function App() {
@@ -8,14 +7,14 @@ function App() {
       <nav>
         {/* use the class list to remove bullet points and align items side-by-side */}
         <ul>
-          <li>hhhhhh</li>
-          <li>About Us</li>
+          <li>home</li>
+          <li>cv</li>
           <li>Contact</li>
         </ul>
       </nav>
       {/* use the class title to change the title color */}
-      <h1>CSS Modules!</h1>
-      <p>This is an example of CSS Modules!</p>
+      <h1>TICON FREDERIC</h1>
+      <p>Developpeur Web</p>
     </div>
   );
 }
