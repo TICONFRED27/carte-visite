cdf82916c3cdd3bb078cf778ec9bac437de8b7ca
FREDERIC TICON <fred.ticon@gmail.com>
2023-10-22 10:28:50 +0200

    Update App.jsx

--- maCarte/src/App.jsx
+++ maCarte/src/App.jsx
@@ -4,17 +4,20 @@ import './style.scss';
 function App() {
   return (
     <div>
+      <h1>TICON FREDERIC</h1>
+      <p>Developpeur Web</p>
       <nav>
         {/* use the class list to remove bullet points and align items side-by-side */}
         <ul>
-          <li>home</li>
-          <li>cv</li>
-          <li>Contact</li>
+          <li>idem1</li>
+          <li>idem2</li>
+          <li>idem3</li>
+          <li>idem4</li>
+          <li>idem5</li>
         </ul>
       </nav>
       {/* use the class title to change the title color */}
-      <h1>TICON FREDERIC</h1>
-      <p>Developpeur Web</p>
+      
     </div>
   );
 }
